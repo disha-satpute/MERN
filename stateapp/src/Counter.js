9871d4e8647576c2c589cfc5f63d4491a8d279a5
Disha Satpute <dishasatpute44@gmail.com>
2024-09-12 22:18:03 +0530

    update counter.js

--- stateapp/src/Counter.js
+++ stateapp/src/Counter.js
@@ -1,23 +1,23 @@
 import { useState } from "react";
- 
+
 //Props: Sending value from parent component to child
 //Whenever state of  component is changes, react automatically renders component
 const Counter=({likes})=>{
     //define state
     const [count, setCount]=useState(likes);
- 
+
     const increment=()=>{
         setCount(count +1);
         console.log(count);
     }
- 
+
     return(
         <div>
             <label>Likes {count}</label>
             <button onClick={increment}>+</button>
         </div>
     );
- 
+
 };
- 
+
 export default Counter;
