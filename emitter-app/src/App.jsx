77d72454793fdb47de86f1dbf9afb34291f49bf9
Disha Satpute <dishasatpute44@gmail.com>
2024-09-28 21:58:55 +0530

    event emmiter added

--- emitter-app/src/App.jsx
+++ emitter-app/src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { useState } from 'react'
+import EventEmitter from 'eventemitter3'
+
+const emitter=new EventEmitter();
+
+function Publisher(){
+  const [text,setText]=useState('');
+
+  const handlechange=(event)=>{
+    setText(event.target.value);
+  };
+  const handleClick=(event)=>{
+    emitter.emit('update',text)
+  }
+  return(
+    <>
+    <h3>
+      publisher
+    </h3>
+    <input type="text" value= {text} onChange={handlechange}/>
+    <button onClick={handleClick}>Publish</button>
+    </>
+  );
+};
+
+
+function Subscriber(){
+  return(
+    <>
+    <h3>
+      Subscriber
+    </h3>
+    <p>Data : </p>
+    </>
+  );
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <> 
+     <div>
+      <h2>Component Communication</h2>
+
+      <table>
+        <tr>
+          <td><Publisher/></td>
+          <td><Subscriber/></td>
+        </tr>
+      </table>
+     </div>
+
+     </>
   )
 }
 
